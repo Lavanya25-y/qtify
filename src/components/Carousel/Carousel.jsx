e6188e696ea850f58c1f9003d34339ebb2388803
Lavanya25-y <incrediblelavanya@gmail.com>
2025-11-25 14:08:38 +0530

    Added all project files

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,51 @@
+
+
+import React, { useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules"; // <-- FIX
+import { useSwiper } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation"; // <-- IMPORTANT
+
+import styles from "./Carousel.module.css";
+
+import CarouselLeftNavigation from "./CarouselLeftNavigation/CarouselLeftNavigation";
+import CarouselRightNavigation from "./CarouselRightNavigation/CarouselRightNavigation";
+
+const Controls = ({ data }) => {
+  const swiper = useSwiper();
+
+  useEffect(() => {
+    swiper.slideTo(0);
+  }, [data]);
+
+  return null;
+};
+
+function Carousel({ data, renderComponent }) {
+  return (
+    <div className={styles.wrapper}>
+      <Swiper
+        style={{ padding: "0px 20px" }}
+        initialSlide={0}
+        modules={[Navigation]}
+        navigation
+        slidesPerView="auto"
+        spaceBetween={40}
+        allowTouchMove
+      >
+        <Controls data={data} />
+
+        <CarouselLeftNavigation />
+        <CarouselRightNavigation />
+
+        {data.map((ele, index) => (
+          <SwiperSlide key={index}>{renderComponent(ele)}</SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
+
+export default Carousel;
